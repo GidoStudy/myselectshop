dd628d1df2e57d6c16d7f66b1ca976a264c04323
jdm2131 <jdm2131@naver.com>
2024-07-30 00:34:18 +0900

    feat : 회원별 상품 API

--- src/main/java/com/sparta/myselectshop/entity/User.java
+++ src/main/java/com/sparta/myselectshop/entity/User.java
@@ -11,8 +11,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false, unique = true)
