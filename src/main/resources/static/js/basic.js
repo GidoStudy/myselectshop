9e10b2181d74487d3cd9137db5a0f0057cd5ca08
jdm2131 <jdm2131@naver.com>
2024-07-31 00:37:36 +0900

    feat : kakao login

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -3,7 +3,6 @@ let targetId;
 let folderTargetId;
 
 $(document).ready(function () {
-
     const auth = getToken();
 
     if (auth !== undefined && auth !== '') {
@@ -429,10 +428,16 @@ function logout() {
 }
 
 function getToken() {
+
     let auth = Cookies.get('Authorization');
 
     if(auth === undefined) {
         return '';
+    }
+
+    // kakao 로그인 사용한 경우 Bearer 추가
+    if(auth.indexOf('Bearer') === -1 && auth !== ''){
+        auth = 'Bearer ' + auth;
     }
 
     return auth;
